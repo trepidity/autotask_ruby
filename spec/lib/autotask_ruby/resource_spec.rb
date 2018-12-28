2779e83c50c7321d77b2648ef2819bc4bbf16e94
Jared L Jennings <jared@jaredjennings.org>
2018-12-28 05:34:54 -0600

    Updated Resource spec to use integration code

--- spec/lib/autotask_ruby/resource_spec.rb
+++ spec/lib/autotask_ruby/resource_spec.rb
@@ -5,14 +5,15 @@ RSpec.describe :resource do
     let(:endpoint) { 'https://webservices2.autotask.net/ATServices/1.5/atws.asmx' }
     let(:valid_api_user) { 'api_user@autotaskdemo.com' }
     let(:valid_password) { 'something' }
-    let(:client) do
-        AutotaskRuby::Client.new(basic_auth: [valid_api_user, valid_password],
-                                 integration_code: ENV['INTEGRATION_CODE'],
-                                 endpoint: endpoint)
-    end
-    let(:subject) { client.find(:resource, 29_684_250) }
 
-    context 'resource' do
+    describe 'resource' do
+        let(:client) do
+            AutotaskRuby::Client.new(basic_auth: [valid_api_user, valid_password],
+                                     integration_code: ENV['INTEGRATION_CODE'],
+                                     endpoint: endpoint)
+        end
+        let(:subject) { client.find(:resource, 29_684_250) }
+
         before do
             stub_api_request(query_xml: body, fixture: 'query_resource_response',
                              env_headers: { integration_code: ENV['INTEGRATION_CODE'] })
@@ -24,13 +25,20 @@ RSpec.describe :resource do
         it { expect(subject.email).to eql('rspecninjatools@example.com') }
     end
 
-    # context 'when a resource has account_to_dos' do
-    #     let(:body) { '<tns:query><sXML><![CDATA[<queryxml><entity>AccountToDo</entity><query><field>AssignedToResourceID<expression op="equals">29684250</expression></field></query></queryxml>]]></sXML></tns:query>' }
-    #
-    #     before do
-    #         stub_api_request({query_xml: body, fixture: 'query_resource_account_to_do_response'})
-    #     end
-    #
-    #     it { expect(subject.account_to_dos.size).to eql(1) }
-    # end
+    describe 'resource has account_to_dos' do
+        let(:body) { '<tns:query><sXML><![CDATA[<queryxml><entity>AccountToDo</entity><query><field>AssignedToResourceID<expression op="equals">29684250</expression></field></query></queryxml>]]></sXML></tns:query>' }
+        let(:client) do
+            AutotaskRuby::Client.new(basic_auth: [valid_api_user, valid_password],
+                                     integration_code: ENV['INTEGRATION_CODE'],
+                                     endpoint: endpoint)
+        end
+        let(:subject) { client.find(:resource, 29_684_250) }
+
+        before do
+            stub_api_request(query_xml: body, fixture: 'query_resource_account_to_do_response',
+                             env_headers: { integration_code: ENV['INTEGRATION_CODE'] })
+        end
+
+        it { expect(subject.account_to_dos.size).to eql(1) }
+    end
 end
