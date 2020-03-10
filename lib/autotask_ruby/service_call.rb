53c90027ff9797d2c0161e93d6871252877a5c6b
Jared L Jennings <jared@jaredjennings.org>
2020-03-10 12:45:40 -0500

    Fixed an error that could cause a NoMethodError

--- lib/autotask_ruby/service_call.rb
+++ lib/autotask_ruby/service_call.rb
@@ -7,15 +7,20 @@ module AutotaskRuby
     include Query
 
     FIELDS = %i[id AccountID StartDateTime EndDateTime Description Complete CreateDateTime LastModifiedDateTime Duration Status].freeze
-                                                                                                                                .each do |field|
+                 .each do |field|
       attr_accessor :"#{field.to_s.underscore}"
     end
 
     # Returns the associated ResourceID.
     def resource_id
       service_call_ticket = find('ServiceCallTicket', 'ServiceCallId', id)
-      service_call_ticket_resource = find('ServiceCallTicketResource', 'ServiceCallTicketId', service_call_ticket.id)
-      service_call_ticket_resource.resource_id
+
+      unless service_call_ticket.nil?
+        service_call_ticket_resource = find('ServiceCallTicketResource', 'ServiceCallTicketId', service_call_ticket.id)
+        return service_call_ticket_resource.resource_id unless service_call_ticket_resource.nil?
+      end
+
+      nil
     end
 
     # Returns the associated Resource
